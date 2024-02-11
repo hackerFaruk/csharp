c7219e0e6ef4d1f8286e351a12fe78c3f9d8c883
hackerFaruk <onuralparslan97@gmail.com>
2024-02-11 21:12:53 +0300

    added new funct to learn  about

--- learn/learn.cs
+++ learn/learn.cs
@@ -9,5 +9,22 @@ class Learn{
         Console.WriteLine("choose application");
         string choice = Console.ReadLine();
         Console.WriteLine("given input is " + choice );
+
+        if(choice == "alm"){ newFunc();}
+        else if( choice =="arr"){arrayLogic();}
+    }
+
+    static void newFunc(){
+        Console.WriteLine("hallo");
+    }
+
+    static void arrayLogic(){
+        int[] value = { 3, 4, 6 };
+        Console.WriteLine("select a val between 0 and " + (value.Length-1));
+        string number = Console.ReadLine();
+        Console.WriteLine(value[int.Parse(s: number)]);
+
+
+      
     }
 }
--- learn/obj/Debug/net8.0/learn.AssemblyInfo.cs
+++ learn/obj/Debug/net8.0/learn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1cdce9447f6a7b620ab24567b220e625e79d7a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a300741acda13d59d7b495651947f27128575d54")]
 [assembly: System.Reflection.AssemblyProductAttribute("learn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
